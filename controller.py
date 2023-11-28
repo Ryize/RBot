8356775320bd6cacf47880681c8e2110e5b54fac
matvejcekasov <chekashovmatvey@gmail.com>
2023-11-28 14:33:11 +0300

    feat: Сделана вёрстка

--- controller.py
+++ controller.py
@@ -1,6 +1,8 @@
+from flask import render_template
+
 from app import *
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def hello_world():
+    return render_template('index.html')
